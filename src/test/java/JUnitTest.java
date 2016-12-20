ab245b26f04d6c06e17c3150d38392f6760f7e2b
SUB_ZERO <JamesAs3@Cardiff.ac.uk>
2016-12-20 02:29:30 +0000

    Renamed file

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -17,7 +17,7 @@ public class JUnitTest extends TestCase{
 
 	public static void testOutput() throws Exception{
 
-            String systemResult = RouterCheck.beginRouterCheck("C:/Users/Sean/Documents/GitHub/BTCodeTest2016/src/main/java/sampleTests.csv");
+            String systemResult = RouterCheck.beginRouterCheck("sampleTests.csv");
 
             String expectedResult = "b.example.com (1.1.1.2), OS version 13 [Behind the other routers so no one sees it]f.example.com (1.1.1.7), OS version 12.200";
 
